762e582b70b4e2b52f9a884b08500a4cb5e757ca
punisher-699 <godakhindiprateek@gmail.com>
2026-06-15 18:07:13 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        maxp = 0
+        bp , sp = 0 , 1
+        while bp != sp and bp <= len(prices) - 1 and sp <= len(prices) - 1:
+            cost =  prices[sp] - prices[bp]
+            if cost > maxp:
+                maxp = cost
+                sp += 1
+            else :
+                if prices[sp] > prices[bp]:
+                    sp += 1
+                else :
+                    sp += 1
+                    bp += 1
+
+            
+        return maxp
