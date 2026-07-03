95447d7ba233c91bafac7283b95ad3fef8563ca8
Frank Castle <godakhindiprateek@gmail.com>
2026-07-03 16:09:03 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+
+        piles.sort()
+
+        if len(piles) == h:
+            return piles[-1]
+        
+        #totalhours = sum(piles)
+
+        #brute force
+        res = float("inf")
+        for pile in range(piles[0],piles[-1]):
+            time = 0
+            for p in piles:
+                if p <= pile:
+                    time += 1
+                else:
+                    time += math.ceil(p / pile)
+            if time <= h:
+                res = min(res, pile)
+        return res
