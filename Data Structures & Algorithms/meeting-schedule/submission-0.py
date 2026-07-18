3d5d369a474adbc5048d4c261e5daef50280cc60
Frank Castle <godakhindiprateek@gmail.com>
2026-07-18 22:48:43 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,19 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+
+        intervals.sort(key = lambda x: x.start)
+        prev = intervals[0].end
+
+        for interval in intervals[1:]:
+            if interval.start < prev:
+                return False
+            prev = interval.end
+        return True
