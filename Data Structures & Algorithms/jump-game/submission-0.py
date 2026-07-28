5530baf88e0590785495babca782a75b3ee6fdc4
Frank Castle <godakhindiprateek@gmail.com>
2026-07-28 22:37:45 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        jr, dest = 0, len(nums) - 1
+        j = 0
+        
+        while j < dest:
+            if nums[j] == 0:
+                return False
+            jr += nums[j]
+            j = jr
+        
+        return jr == dest
+
